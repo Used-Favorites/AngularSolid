c4310b58fb450ce3774f8fcdc7de598f53228787
Gabriel Oliveira <40695127+Polabiel@users.noreply.github.com>
2024-06-16 08:05:29 -0300

    refactor: Update button component to use Ionic framework

--- src/app/components/button-default/button-default.component.ts
+++ src/app/components/button-default/button-default.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonButton, IonImg } from '@ionic/angular/standalone';
+import { IonButton, IonImg, IonTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-button-default',
   standalone: true,
-  imports: [IonImg, IonButton, CommonModule],
+  imports: [IonTitle, IonImg, IonButton, CommonModule],
   templateUrl: './button-default.component.html',
   styleUrl: './button-default.component.scss',
 })
