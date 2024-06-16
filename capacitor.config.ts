f8ca529ecead78aada4cc0477f3ba193dc9d5efb
Gabriel Oliveira <40695127+Polabiel@users.noreply.github.com>
2024-06-16 08:05:23 -0300

    refactor: Update import paths for button component, login page, and extensions.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,26 +1,26 @@
-import { execSync } from "child_process";
+import { execSync } from 'child_process';
 import type { CapacitorConfig } from '@capacitor/cli';
 
 import * as os from 'os';
 const getIPAddress = (): string => {
   try {
-    let command = "ipconfig getifaddr en0";
+    let command = 'ipconfig getifaddr en0';
     if (os.platform() === 'win32') {
       command = `for /f "tokens=2 delims=:" %a in ('ipconfig ^| findstr /C:"IPv4"') do @echo %a`;
     }
     const ipAddress = execSync(command).toString().trim();
     return ipAddress;
   } catch (error) {
-    console.error("Erro ao obter o endereço IP:", error);
-    return "";
+    console.error('Erro ao obter o endereço IP:', error);
+    return '';
   }
 };
 
 const config: CapacitorConfig = {
   appId: 'com.angular.usedfavorites',
   appName: 'AngularFlare',
-  webDir: 'www',
-  bundledWebRuntime: false,
+  webDir: 'dist/browser',
+  bundledWebRuntime: true,
   server: {
     url: `http://${getIPAddress()}:4200`,
     cleartext: true,
