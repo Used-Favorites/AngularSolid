673fad39dd08f5fe42ba7028df4333aef46cf99a
Gabriel <bielgabrieloliveira77@gmail.com>
2024-06-21 15:22:49 -0300

    refactor: Update app name to "Used Favorites" in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,19 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.angular.solid',
-  appName: 'AngularSolid',
+  appName: 'Used Favorites',
   webDir: 'dist/browser',
-  cordova: {
-    preferences: {
-      ScrollEnabled: 'false',
-      BackupWebStorage: 'none',
-      SplashMaintainAspectRatio: 'true',
-      FadeSplashScreenDuration: '300',
-      SplashShowOnlyFirstTime: 'false',
-      SplashScreen: 'screen',
-      SplashScreenDelay: '3000',
-    },
-  },
 };
 
 export default config;
