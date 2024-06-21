9355d9ff12adf8253525eeb8b11a38753ce16156
Gabriel <bielgabrieloliveira77@gmail.com>
2024-06-21 11:22:17 -0300

    refactor: Remove unused data service and related files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,32 +1,10 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
-import { execSync } from 'child_process';
-
-import * as os from 'os';
-const getIPAddress = (): string => {
-  try {
-    let command = 'ifconfig getifaddr en0';
-    if (os.platform() === 'win32') {
-      command = `for /f "tokens=2 delims=:" %a in ('ipconfig ^| findstr /C:"IPv4"') do @echo %a`;
-    }
-    const ipAddress = execSync(command).toString().trim();
-    console.log('Endereço IP:', ipAddress);
-    return ipAddress;
-  } catch (error) {
-    console.error('Erro ao obter o endereço IP:', error);
-    return '';
-  }
-};
-
 const config: CapacitorConfig = {
   appId: 'com.angular.solid',
   appName: 'AngularSolid',
   webDir: 'dist',
   bundledWebRuntime: false,
-  server: {
-    url: `http://${getIPAddress()}:8100`,
-    cleartext: true,
-  },
   cordova: {
     preferences: {
       ScrollEnabled: 'false',
