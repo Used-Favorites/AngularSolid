e5ca1faa41a53b5b57153ace10d24ce0899bf940
Gabriel <bielgabrieloliveira77@gmail.com>
2024-06-21 11:19:01 -0300

    refactor: Update import paths for button component, login page, and extensions.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,16 @@
-import { execSync } from 'child_process';
 import type { CapacitorConfig } from '@capacitor/cli';
+
+import { execSync } from 'child_process';
 
 import * as os from 'os';
 const getIPAddress = (): string => {
   try {
-    let command = 'ipconfig getifaddr en0';
+    let command = 'ifconfig getifaddr en0';
     if (os.platform() === 'win32') {
       command = `for /f "tokens=2 delims=:" %a in ('ipconfig ^| findstr /C:"IPv4"') do @echo %a`;
     }
     const ipAddress = execSync(command).toString().trim();
+    console.log('Endereço IP:', ipAddress);
     return ipAddress;
   } catch (error) {
     console.error('Erro ao obter o endereço IP:', error);
@@ -17,13 +19,24 @@ const getIPAddress = (): string => {
 };
 
 const config: CapacitorConfig = {
-  appId: 'com.angular.usedfavorites',
-  appName: 'AngularFlare',
-  webDir: 'dist/browser',
-  bundledWebRuntime: true,
+  appId: 'com.angular.solid',
+  appName: 'AngularSolid',
+  webDir: 'dist',
+  bundledWebRuntime: false,
   server: {
-    url: `http://${getIPAddress()}:4200`,
+    url: `http://${getIPAddress()}:8100`,
     cleartext: true,
+  },
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000',
+    },
   },
 };
 
